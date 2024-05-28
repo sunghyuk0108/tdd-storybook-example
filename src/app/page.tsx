da7da3c9f0a0c2236d7fb2e21d00e7d8bcc41bc3
sunghyuk930108 <harlie0108@gmail.com>
2024-05-28 19:11:34 +0900

    feat: test applying use client to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+'use client';
+import React from 'react';
 import TestButton from '@/shared/components/test-button';
-
 export default function Home() {
   return (
     <main className="main-container">
       <h1>tdd-storybook-example</h1>
       <div className="button-wrap">
         <h2>button components</h2>
-        <TestButton label="test button" intent="primary" />
+        <TestButton
+          label="test button"
+          intent="primary"
+          onClick={() => {
+            console.log('primary bt click');
+          }}
+        />
         <TestButton label="test button" intent="secondary" />
         <TestButton label="test button" intent="destructive" />
         <TestButton label="test button" intent="secondary" disabled={true} />
--- src/shared/components/test-button/index.tsx
+++ src/shared/components/test-button/index.tsx
@@ -5,16 +5,12 @@ import './index.scss';
 type TestButtonProps = {
   label?: string | React.ReactNode;
   intent?: 'primary' | 'secondary' | 'destructive';
-  onClick?: () => void;
+  onClick?: (event: React.MouseEvent) => void;
 } & Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'>;
 
 // otherProps외에 꺼내쓰면 제외됨
-const TestButton: React.FC<TestButtonProps> = ({
-  label,
-  onClick,
-  intent,
-  ...otherProps
-}) => {
+const TestButton = (props: TestButtonProps) => {
+  const { label, onClick, intent, ...otherProps } = props;
   return (
     <button
       className="test-button"
