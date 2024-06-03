8222700aeb1a31a86be04c4b661983e9d8ea1a00
sunghyuk930108 <harlie0108@gmail.com>
2024-06-03 10:49:25 +0900

    fix: modify page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,12 @@
 'use client';
 import React from 'react';
 import TestButton from '@/shared/components/test-button';
+import ButtonsWidget from '../widgets/buttons-widget';
 export default function Home() {
   return (
     <main className="main-container">
       <h1>tdd-storybook-example</h1>
-      <div className="button-wrap">
-        <h2>button components</h2>
-        <TestButton
-          label="test button"
-          intent="primary"
-          onClick={() => {
-            console.log('primary bt click');
-          }}
-        />
-        <TestButton label="test button" intent="secondary" />
-        <TestButton label="test button" intent="destructive" />
-        <TestButton label="test button" intent="secondary" disabled={true} />
-      </div>
+      <ButtonsWidget />
     </main>
   );
 }
